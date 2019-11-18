6aabd95ac42c5079d5db8f2a2085b2e18f9cdf01
Akash <akash.lnu@idemia.com>
2019-11-18 08:35:09 +0530

    reactive with map and flatMap parallel execution

--- src/test/java/in/co/bytehub/learn/reactive/FluxWithStreams.java
+++ src/test/java/in/co/bytehub/learn/reactive/FluxWithStreams.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 public class FluxWithStreams {
@@ -37,7 +38,7 @@ public class FluxWithStreams {
 
         // We need to flatten version of this for further processing easily
 
-        Flux<String> stringFlux = mapFlux.flatMap(x -> x);
+        Flux<String> stringFlux = mapFlux.flatMap(x -> x).log();
 
         StepVerifier.create(stringFlux)
                 .expectNext("Nikita", "NIKITA")
@@ -48,6 +49,50 @@ public class FluxWithStreams {
     }
 
     private Flux<String> externalServiceCall(String name) {
+        try {
+            Thread.sleep(1000); // Just to take the feel of external call
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return Flux.fromIterable(Arrays.asList(name, name.toUpperCase()));
+    }
+
+    @Test
+    public void prefWithFlatMap() {
+        // This approach took 4 seconds to complete the operation as external service is taking
+        // one second on each call
+        // And calls are in sequence
+        Flux<String> stringFlux = Flux.fromIterable(nameList)
+                .flatMap(this::externalServiceCall).log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Nikita", "NIKITA")
+                .expectNext("Nishant", "NISHANT")
+                .expectNext("David", "DAVID")
+                .expectNext("Kali", "KALI")
+                .verifyComplete();
+    }
+
+    @Test
+    public void parallelExecution() {
+
+        Flux<String> stringFlux = Flux.fromIterable(nameList)
+                .window(2) // It transform Whole flux in Flux of chunk of Flux i.e Flux<Flux<String>>
+                .map(x -> x.map(t -> externalServiceCall(t))
+                        .subscribeOn(Schedulers.parallel()))
+                .flatMap(z -> z)
+                .flatMap(z -> z)
+                .log();
+
+
+        /* Little bit nicer way */
+//        Flux<String> stringFlux = Flux.fromIterable(nameList)
+//                .window(2)
+//                .flatMap(x -> x.flatMap(t -> externalServiceCall(t)).subscribeOn(Schedulers.parallel()))
+//                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNextCount(8)
+                .verifyComplete();
     }
 }
