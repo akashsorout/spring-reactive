aacf8bf808e2b8e9d18d039a288a2f363a0e9093
Akash <akash.lnu@idemia.com>
2019-11-20 09:32:07 +0530

    Introduced basic hello endpoint
    Signed-off-by: Akash <akash.lnu@idemia.com>

---
+++ src/main/java/in/co/bytehub/learn/reactive/controller/HelloController.java
@@ -0,0 +1,13 @@
+package in.co.bytehub.learn.reactive.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+}
--- src/test/java/in/co/bytehub/learn/reactive/FluxErrorHandling.java
+++ src/test/java/in/co/bytehub/learn/reactive/FluxErrorHandling.java
@@ -10,7 +10,7 @@ public class FluxErrorHandling {
     public void fluxHandleErrors() {
         Flux<String> fluxWithException = Flux.just("1", "2", "3")
                 .concatWith(Flux.error(new RuntimeException("Exception occured in flux")))
-                .onErrorResume(e -> Flux.just("After Error Resume"))
+                .onErrorResume(e -> Flux.just("After Error Resume")) // Will execute when an Error occurred
                 .concatWith(Flux.just("4"));
 
         StepVerifier.create(fluxWithException)
